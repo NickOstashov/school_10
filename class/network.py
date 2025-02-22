7d179024ff3867bfab5cd0ae34171269b8312125
NickOstashov <111501728+NickOstashov@users.noreply.github.com>
2025-02-22 14:37:23 +0500

    upd

--- class/network.py
+++ class/network.py
@@ -3,10 +3,12 @@ import uuid
 
 class User:
     def __init__(self, name, user_password, user_mail):
+        # свойства из параметров
         self.user_name = name
         self.password = user_password
         self.mail = user_mail
 
+        # свойства задаваемые автоматически при создании экземляра
         self.friend_list = list()
         self.messages = list()
         self.uuid = str(uuid.uuid4())
@@ -19,6 +21,18 @@ class User:
         print("messages: ", self.messages)
         print("uuid: ", self.uuid)
 
-user_1 = User("Вася Пупкин", "123", "vasia@mail.ru")
-user_1.print_info()
+    def add_friend(self, other_user : "User"):
+        self.friend_list.append(other_user)
+        other_user.friend_list.append(self)
 
+user_1 = User("Вася Пупкин", "123", "vasia@mail.ru")
+
+user_2 = User("Федя", "321", "fedua@mail.ru")
+
+user_1.add_friend(user_2)
+
+print("USER_1 INFO")
+user_1.print_info()
+print()
+print("USER_2 INFO")
+user_2.print_info()
