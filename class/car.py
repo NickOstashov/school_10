d9126a208638b535f5cf245667e5ba49e22fd9dd
NickOstashov <111501728+NickOstashov@users.noreply.github.com>
2025-02-22 14:17:01 +0500

    create_class

---
+++ class/car.py
@@ -0,0 +1,19 @@
+class Car:
+    def __init__(self, color, model, door_number):
+        self.color = color
+        self.model = model
+        self.door_number = door_number
+
+    def print_info(self):
+        print("цвет :", self.color)
+        print("модель :", self.model)
+        print("количетсво дверей :", self.door_number)
+
+print("CAR_1")
+car_1 = Car("желтый", "lada", 4)
+car_1.print_info()
+
+print()
+print("CAR_2")
+car_2 = Car("Черный", "УАЗ", 9)
+car_2.print_info()
