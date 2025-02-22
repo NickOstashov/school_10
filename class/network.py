74d776035ecbc6ef2282abfccce3fc264b7554b0
NickOstashov <111501728+NickOstashov@users.noreply.github.com>
2025-02-22 14:42:04 +0500

    upd

--- class/network.py
+++ class/network.py
@@ -22,8 +22,16 @@ class User:
         print("uuid: ", self.uuid)
 
     def add_friend(self, other_user : "User"):
+        # добавление в свой список друзей другого пользователя
         self.friend_list.append(other_user)
+        # удаление себя из списка друзей у другого пользователя
         other_user.friend_list.append(self)
+
+    def remove_friend(self, other_user : "User"):
+        # удаление пользователя из своего списка друзей
+        self.friend_list.remove(other_user)
+        # удаление себя из списка друзей у другого пользователя
+        other_user.friend_list.remove(self)
 
 user_1 = User("Вася Пупкин", "123", "vasia@mail.ru")
 
@@ -31,6 +39,14 @@ user_2 = User("Федя", "321", "fedua@mail.ru")
 
 user_1.add_friend(user_2)
 
+print("USER_1 INFO")
+user_1.print_info()
+print()
+print("USER_2 INFO")
+user_2.print_info()
+print()
+user_1.remove_friend(user_2)
+print('after_remove')
 print("USER_1 INFO")
 user_1.print_info()
 print()
